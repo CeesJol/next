7cfd0c25f654d5d7da44234498f7995712fe6ddc
Cees <cjol99@icloud.com>
2020-06-04 09:15:08 +0200

    Store confirmation locally

--- pages/confirm/[token].js
+++ pages/confirm/[token].js
@@ -1,13 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { useRouter } from "next/router";
 import jwt from "jsonwebtoken";
 import { confirm } from "../api/confirm";
 import Router from "next/router";
+import UserContext from "../../contexts/userContext";
 
 export default function Token() {
   const router = useRouter();
   const { token } = router.query;
-  const [status, setStatus] = useState("Confirming...");
+	const [status, setStatus] = useState("Confirming...");
+	const { user, userExists, storeUser, getUser } = useContext(UserContext);
 
   useEffect(() => {
     if (token) {
@@ -15,6 +17,9 @@ export default function Token() {
         (data) => {
           console.log("data", data);
 					setStatus("Email confirmed successfully!");
+					storeUser({
+						confirmed: true
+					})
 					Router.push("/dashboard");
         },
         (err) => {
